0c811575c733844a9329144259fab1872c6c295e
Hubert <hrozmarynowski776@gmail.com>
2022-06-03 20:43:04 +0200

    Initial commit

---
+++ js/content.js
@@ -0,0 +1,11 @@
+var images = document.getElementsByTagName('img');
+const l = images.length;
+
+for (var i = l; i > 0; i--) {
+    if (images[i] != undefined) {
+        if (images[i].src.startsWith("https://matematykaszkolna.pl/forum/emots/")) {
+            images[i].remove()
+            //images[i].parentNode.removeChild(images[i]);
+        }
+    }
+}
